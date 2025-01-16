fa7d7fab185412a56ea36326f9564563788bac95
GitHubmandan850 <159153@student.vansd.org>
2025-01-15 16:58:20 -0800

    added main game

---
+++ Main Game/Assets/PlayerController.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator playerAnim;
+    public Rigidbody playerRigid;
+    public float w_speed, wb_speed, olw_speed, rn_speed, ro_speed;
+    public float jumpSpeed;
+    public bool walking;
+    public Transform playerTrans;
+
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            playerRigid.velocity = transform.forward * w_speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            playerRigid.velocity = -transform.forward * wb_speed * Time.deltaTime;
+        }
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            playerAnim.SetTrigger("Walk");
+            playerAnim.ResetTrigger("Idle");
+            walking = true;
+        }
+        if(Input.GetKeyUp(KeyCode.W))
+        {
+            playerAnim.ResetTrigger("Walk");
+            playerAnim.SetTrigger("Idle");
+            walking = false;
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            playerAnim.SetTrigger("Walk Back");
+            playerAnim.ResetTrigger("Idle");
+        }
+        if(Input.GetKeyUp(KeyCode.S))
+        {
+            playerAnim.ResetTrigger("Walk Back");
+            playerAnim.SetTrigger("Idle");
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            playerTrans.Rotate(0, -ro_speed * Time.deltaTime, 0);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            playerTrans.Rotate(0, ro_speed * Time.deltaTime, 0);
+        }
+        if(walking == true)
+        {
+            if(Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                w_speed = w_speed + rn_speed;
+                playerAnim.SetTrigger("Run");
+                playerAnim.ResetTrigger("Walk");
+            }
+            if(Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                w_speed = olw_speed;
+                playerAnim.ResetTrigger("Run");
+                playerAnim.SetTrigger("Walk");
+            }
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            playerRigid.AddForce(Vector3.up * jumpSpeed);
+            playerAnim.SetTrigger("Jump");
+            playerAnim.ResetTrigger("Idle");
+        }
+        if (Input.GetKeyUp("space"))
+        {
+            playerAnim.ResetTrigger("Jump");
+            playerAnim.SetTrigger("Idle");
+        }
+
+    }
+}
