622479592aeb21a7f528dc4dbadce3844fc49222
GitHubmandan850 <159153@student.vansd.org>
2025-03-14 15:21:13 -0700

    Danny changes

---
+++ Main Game/Assets/InventoryManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public GameObject inventoryMenu;
+    private bool menuactivated;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Inventory") && menuactivated)
+        {
+            Time.timeScale = 1;
+            inventoryMenu.SetActive(false);
+            menuactivated = false;
+        }
+        else if(Input.GetButtonDown("Inventory") && !menuactivated)
+        {
+            Time.timeScale = 0;
+            inventoryMenu.SetActive(true);
+            menuactivated = true;
+        }
+    }
+}
